55ecbbe90918ed464dec36df30375516cab28939
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2022-05-26 22:34:38 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import signal
+
+import requests
+
+from telegram import Update
+
+from telegram.ext import Updater
+from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+SLACK_TOKEN = os.getenv('SLACK_TOKEN')
+
+
+headers = {
+    'Authorization': f'Bearer {SLACK_TOKEN}'
+}
+
+url = 'https://slack.com/api/chat.postMessage'
+
+
+# mrkdwn_template = """
+# > {text}
+
+# {link}
+# """.strip()
+
+mrkdwn_template = '{link}'
+
+
+def post(text, link, channel):
+    mrkdwn = mrkdwn_template.format(text=text, link=link)
+
+    message = {
+      "channel": channel,
+      "blocks": [
+        {"type": "section", "text": {"type": "mrkdwn", "text": mrkdwn}},
+      ]
+    }
+
+    response = requests.post(url, json=message, headers=headers).json()
+    return response
+
+
+
+
+channel_mapping = {
+    -1001730331343: {
+        'channel': 'course-mlops-zoomcamp',
+        'link': 'https://t.me/dtc_courses'
+    },
+    -1001707092787: {
+        'channel': 'integration_test',
+        'link': 'https://t.me/dtc_test'
+    }
+}
+
+
+
+def capture(update: Update, context: CallbackContext):
+    print('processing another message...')
+    print(update)
+    
+    if update.channel_post is None:
+        print('not a channel post')
+        return
+
+    message = update.channel_post
+    chat_id = message.chat.id
+    
+    if chat_id not in channel_mapping:
+        print(f'unknown chat_id = {chat_id}')
+        return
+
+    
+    send_info = channel_mapping[chat_id]
+
+    url_prefix = send_info['link']
+    message_id = message.message_id
+
+    link = f"{url_prefix}/{message_id}"
+
+    response = post(message.text, link, send_info['channel'])
+    print(response)
+
+
+
+updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
+
+dispatcher = updater.dispatcher
+
+capture_text_handler = MessageHandler(Filters.text & (~Filters.command), capture)
+dispatcher.add_handler(capture_text_handler)
+
+
+print('starting listening...')
+updater.start_polling()
+updater.idle([signal.SIGINT])
+
+
+
+
